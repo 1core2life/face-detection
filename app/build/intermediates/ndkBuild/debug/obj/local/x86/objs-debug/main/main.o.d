63183891084b05df76fdd6491ed5b59d201b9a6f
1core2life <whitezonen1@gmail.com>
2018-03-19 01:19:17 +0900

    8 fitler n change size

---
+++ app/build/intermediates/ndkBuild/debug/obj/local/x86/objs-debug/main/main.o.d
@@ -0,0 +1,188 @@
+C:/Users/b/Desktop/and_face/facedetection/app/build/intermediates/ndkBuild/debug/obj/local/x86/objs-debug/main/main.o: \
+  C:/Users/b/Desktop/and_face/facedetection/app/src/main/jni/main.cpp \
+  C:/Users/b/Desktop/and_face/facedetection/app/src/main/jni/com_example_b_facedetection_MainActivity.h \
+  C:/Users/b/Desktop/and_face/facedetection/app/src/main/jni/Point.h \
+  C:/Users/b/Desktop/and_face/facedetection/app/src/main/jni/Scalar.h \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\stdlib.h \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\string.h \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\stdio.h \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\math.h \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\fstream \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\ostream \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\ios \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\iosfwd \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\wchar.h \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__locale \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\string \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\string_view \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__string \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\algorithm \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\initializer_list \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstring \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\utility \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__tuple \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdint \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__debug \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\memory \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\typeinfo \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\exception \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdlib \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\new \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\iterator \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__functional_base \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\tuple \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\stdexcept \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cassert \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\atomic \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdio \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cwchar \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cwctype \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cctype \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\ctype.h \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\wctype.h \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\mutex \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__mutex_base \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\chrono \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\ctime \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\ratio \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\climits \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\system_error \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cerrno \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__threading_support \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\functional \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\locale.h \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\support/android/locale_bionic.h \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\support/xlocale/__posix_l_fallback.h \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\support/xlocale/__strtonum_fallback.h \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\streambuf \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\locale \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdarg \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__bsd_locale_fallbacks.h \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\bitset \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__bit_reference \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\istream \
+  C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\iostream
+
+C:/Users/b/Desktop/and_face/facedetection/app/src/main/jni/com_example_b_facedetection_MainActivity.h:
+
+C:/Users/b/Desktop/and_face/facedetection/app/src/main/jni/Point.h:
+
+C:/Users/b/Desktop/and_face/facedetection/app/src/main/jni/Scalar.h:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\stdlib.h:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\string.h:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\stdio.h:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\math.h:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\fstream:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\ostream:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\ios:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\iosfwd:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\wchar.h:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__locale:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\string:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\string_view:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__string:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\algorithm:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\initializer_list:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstring:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\utility:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__tuple:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdint:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__debug:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\memory:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\typeinfo:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\exception:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdlib:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\new:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\iterator:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__functional_base:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\tuple:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\stdexcept:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cassert:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\atomic:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdio:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cwchar:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cwctype:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cctype:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\ctype.h:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\wctype.h:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\mutex:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__mutex_base:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\chrono:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\ctime:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\ratio:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\climits:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\system_error:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cerrno:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__threading_support:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\functional:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\locale.h:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\support/android/locale_bionic.h:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\support/xlocale/__posix_l_fallback.h:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\support/xlocale/__strtonum_fallback.h:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\streambuf:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\locale:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\cstdarg:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__bsd_locale_fallbacks.h:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\bitset:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\__bit_reference:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\istream:
+
+C:/Users/b/AppData/Local/Android/Sdk/ndk-bundle/build//../sources/cxx-stl/llvm-libc++/include\iostream:
